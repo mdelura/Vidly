ca0bc9dd9eeacf58aafeafc17cfc6d9947dd379f
Michal Delura <deluramichal@gmail.com>
2018-02-13 23:48:48 +0100

    Enable Code-First migrations, update CustomersController.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,30 +11,33 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        List<Customer> _customers = new List<Customer>()
+        private ApplicationDbContext _context;
+
+        public CustomersController()
         {
-            new Customer { Id = 0, Name = "John Smith"},
-            new Customer { Id = 1, Name = "Mary Williams"},
-            new Customer { Id = 2, Name = "Ryan Johnson"},
-        };
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
         // GET: Customers
         public ActionResult Index()
         {
+            var customers = _context.Customers.Include(c => c.MembershipType);
 
-            var customersViewModel = new CustomersViewModel()
-            {
-                Items = _customers
-            };
 
-            return View(customersViewModel);
+            return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = _customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer != null)
                 return View(customer);
+
             return HttpNotFound();
         }
     }
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -21,12 +21,7 @@ namespace Vidly.Controllers
         // GET: Movies
         public ActionResult Index()
         {
-            var moviesViewModel = new MoviesViewModel()
-            {
-                Items = _movies
-            };
-
-            return View(moviesViewModel);
+            return View(_movies);
         }
 
         public ActionResult Details()
