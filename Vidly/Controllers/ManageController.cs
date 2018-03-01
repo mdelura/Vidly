2cce216d255d8d102c395da63981da46d437b640
Michal Delura <deluramichal@gmail.com>
2018-03-01 18:26:00 +0100

    Fix refactored Vidly.Models.Account namespace issue

--- Vidly/Controllers/ManageController.cs
+++ Vidly/Controllers/ManageController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using Vidly.Models;
 using Vidly.Models.Account;
 
 namespace Vidly.Controllers
